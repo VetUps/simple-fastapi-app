26f5ed3111dc527e5cca0e8b19f1efa85a3a6318
VetUps <sercherufa@gmail.com>
2026-09-17 12:43:55 +0500

    фикстура асинхронного клиента

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,14 +1,16 @@
 import pytest
+from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from app.config import settings
 from app.models import Base
+from app.main import app
 
 async_enige = create_async_engine(url=str(settings.TEST_DATABASE_URL))
 AsyncSessionFactory = async_sessionmaker(bind=async_enige, autoflush=False, expire_on_commit=False)
 
 @pytest.fixture(scope="session", autouse=True)
-async def get_test_db():
+async def setup_database():
     async with async_enige.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
@@ -18,3 +20,11 @@ async def get_test_db():
         await conn.run_sync(Base.metadata.drop_all)
 
     await async_enige.dispose()       
+
+@pytest.fixture
+async def client():
+    transport = ASGITransport(app)
+
+    async with AsyncClient(transport=transport, base_url="http://test") as async_client:
+        yield async_client
+        
