0d305eea79517dc19d44f625f72a56f1457270c2
VetUps <sercherufa@gmail.com>
2026-09-26 12:41:12 +0500

    мок метода проверки существования у пользователей
    доп мок-и в unit-тестах в связи с разделением ответственности методов репоизториев на запрос данных и на запрос факта существования

--- app/tests/unit/test_user_service.py
+++ app/tests/unit/test_user_service.py
@@ -8,30 +8,35 @@ async def test_create_user():
     mock_db = MagicMock()
 
     raw_password = "123456"
-    mock_user = MagicMock()
-    mock_user.user_email = "mockeduser@gmail.com"
-    mock_user.model_dump.return_value = {"user_email": "mockeduser@gmail.com", "user_password": raw_password}
+    user_email = "mockeduser@gmail.com"
+    user_data = {"user_email": user_email, "user_password": raw_password}
 
-    with patch("app.services.users.UserRepository.get_by_email", new=AsyncMock(return_value=None)) as mock_get_by_email, \
+    mock_user = MagicMock()
+    mock_user.user_email = user_email
+    mock_user.model_dump.return_value = user_data
+
+    with patch("app.services.users.UserRepository.is_exists_by_email", new=AsyncMock(return_value=False)) as mock_is_exists_email, \
         patch("app.services.users.security.hash_password", new=AsyncMock(return_value="hashed_passowrd")) as mock_hash_password, \
         patch("app.services.users.UserRepository.craete", new=AsyncMock(return_value=mock_user)) as mock_create:
         result = await UserService.create_user(mock_db, mock_user)
 
         assert result == mock_user
 
-        mock_get_by_email.assert_called_once_with(mock_db, "mockeduser@gmail.com")
+        mock_is_exists_email.assert_called_once_with(mock_db, user_email)
         mock_hash_password.assert_called_once_with(raw_password)
-        mock_create.assert_called_once_with(mock_db, {"user_email": "mockeduser@gmail.com", "user_password": "hashed_passowrd"})
+        mock_create.assert_called_once_with(mock_db, user_data)
 
 async def test_create_already_existed_user():
     mock_db = MagicMock()
 
-    mock_user_data = {"user_email": "mockeduser@gmail.com", "user_password": "123456"}
-    mock_user = MagicMock()
-    mock_user.user_email = "mockeduser@gmail.com"
-    mock_user.model_dump.return_value = mock_user_data
+    user_email = "mockeduser@gmail.com"
+    user_data = {"user_email": user_email, "user_password": "123456"}
 
-    with patch("app.services.users.UserRepository.get_by_email", new=AsyncMock(return_value=mock_user_data)):
+    mock_user = MagicMock()
+    mock_user.user_email = user_email
+    mock_user.model_dump.return_value = user_data
+
+    with patch("app.services.users.UserRepository.is_exists_by_email", new=AsyncMock(return_value=True)):
         with pytest.raises(HTTPException) as exc_info:
             await UserService.create_user(mock_db, mock_user)
 
