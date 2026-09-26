98508727573b2e389cc2e5df1e81007fcf5a07e6
VetUps <sercherufa@gmail.com>
2026-09-26 12:35:07 +0500

    мок метода проверки существования

--- app/tests/unit/test_post_service.py
+++ app/tests/unit/test_post_service.py
@@ -23,13 +23,15 @@ async def test_update_post():
     mock_current_user = MagicMock()
     mock_current_user.user_id = current_user_id
 
-    with patch("app.services.posts.PostRepository.get_by_id", new=AsyncMock(return_value=mock_post_to_update)) as mock_get_by_id, \
+    with patch("app.services.posts.PostRepository.is_exists", new=AsyncMock(return_value=True)) as mock_is_exists, \
+        patch("app.services.posts.PostRepository.get_by_id", new=AsyncMock(return_value=mock_post_to_update)) as mock_get_by_id, \
         patch("app.services.posts.PostRepository.update", new=AsyncMock(return_value=mock_post_new)) as mock_update:
 
         result = await PostService.update_post(mock_db, post_id, mock_post_new, mock_current_user)
 
         assert result == mock_post_new
 
+        mock_is_exists.assert_called_once_with(mock_db, post_id)
         mock_get_by_id.assert_called_once_with(mock_db, post_id)
         mock_update.assert_called_once_with(mock_db, post_data, post_id)
 
@@ -41,12 +43,12 @@ async def test_update_non_existent_post():
     mock_post_new = MagicMock()
     mock_current_user = MagicMock()
 
-    with patch("app.services.posts.PostRepository.get_by_id", new=AsyncMock(return_value=None)) as mock_get_by_id:
+    with patch("app.services.posts.PostRepository.is_exists", new=AsyncMock(return_value=False)) as mock_is_exists:
         with pytest.raises(HTTPException) as exc_info:
             await PostService.update_post(mock_db, post_id, mock_post_new, mock_current_user)
 
         assert exc_info.value.status_code == 404
-        mock_get_by_id.assert_called_once_with(mock_db, post_id)
+        mock_is_exists.assert_called_once_with(mock_db, post_id)
 
 async def test_update_post_by_stranger():
     mock_db = MagicMock()
@@ -63,9 +65,12 @@ async def test_update_post_by_stranger():
     mock_current_user = MagicMock()
     mock_current_user.user_id = current_user_id
 
-    with patch("app.services.posts.PostRepository.get_by_id", new=AsyncMock(return_value=mock_post_to_update)) as mock_get_by_id:
+    with patch("app.services.posts.PostRepository.is_exists", new=AsyncMock(return_value=True)) as mock_is_exists, \
+        patch("app.services.posts.PostRepository.get_by_id", new=AsyncMock(return_value=mock_post_to_update)) as mock_get_by_id:
+
         with pytest.raises(HTTPException) as exc_info:
             await PostService.update_post(mock_db, post_id, mock_post_new, mock_current_user)
 
         assert exc_info.value.status_code == 403
+        mock_is_exists.assert_called_once_with(mock_db, post_id)
         mock_get_by_id.assert_called_once_with(mock_db, post_id)
